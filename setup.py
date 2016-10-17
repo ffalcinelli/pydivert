cb42afa617507fde928cedc48ed4e6c2baa9b1f2
Fabio Falcinelli <fabio.falcinelli@gmail.com>
2016-10-17 13:22:13 +0200

    Add LGPLv3 preamble to each source file as required by the license itself (see  https://www.gnu.org/licenses/gpl-faq.html#GPLOmitPreamble )

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Copyright (C) 2016  Fabio Falcinelli
+# Copyright (C) 2016  Fabio Falcinelli, Maximilian Hils
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
