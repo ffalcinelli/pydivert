fc5286ae9f9a82ba09fe1af26ab5b6bd33dc6809
Fabio Falcinelli <fabio.falcinelli@gmail.com>
2017-02-08 15:00:10 +0100

    Adding python 3.6 to test environments and to supported platforms

--- setup.py
+++ setup.py
@@ -57,6 +57,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: System :: Networking :: Firewalls',
         'Topic :: System :: Networking :: Monitoring',
